7b28cb9345ced2805a6190978fea1cff42531d23
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-12-07 21:54:22 +0300

    Implement WelcomeController class

---
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -0,0 +1,11 @@
+package hexlet.code.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/welcome")
+    public String welcome() {return "Welcome to Spring";}
+}
