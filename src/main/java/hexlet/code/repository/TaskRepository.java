863ac825255577cffbf40002bbb30895e7f728a4
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-12-26 20:36:26 +0300

    Implement TaskService class

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,13 +1,18 @@
 package hexlet.code.repository;
 
 import hexlet.code.model.Task;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
     @Query("SELECT CASE WHEN EXISTS "
             + "(SELECT 1 FROM Task t WHERE t.assignee.id = :userId) THEN true ELSE false END"
@@ -18,5 +23,14 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
             + "(SELECT 1 FROM Task t WHERE t.taskStatus.id = :taskStatusId) THEN true ELSE false END"
     )
     boolean existsByTaskStatusId(@Param("taskStatusId") Long taskStatusId);
+
+    boolean existsByIndex(Long index);
+
+    @Query("SELECT t FROM Task t")
+    @EntityGraph(attributePaths = {"taskStatus", "assignee"})
+    List<Task> findAllWithEagerRelationships();
+
+    @EntityGraph(attributePaths = {"taskStatus", "assignee"})
+    Optional<Task> findWithRelationsById(Long id);
 
 }
---
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -0,0 +1,51 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.task.TaskCreateDto;
+import hexlet.code.dto.task.TaskUpdateDto;
+import hexlet.code.exception.UniquenessViolationException;
+import hexlet.code.mapper.TaskMapper;
+import hexlet.code.model.Task;
+import hexlet.code.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public final class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    private final TaskMapper taskMapper;
+
+    public Task create(TaskCreateDto createDto) {
+        var newTask = taskMapper.toDomain(createDto);
+        return taskRepository.save(newTask);
+    }
+
+    public Optional<Task> getById(Long id) {
+        return taskRepository.findWithRelationsById(id);
+    }
+
+    public List<Task> getAll() {
+        return taskRepository.findAllWithEagerRelationships();
+    }
+
+    public Optional<Task> update(Long id, TaskUpdateDto updateDto) {
+        updateDto.getIndex().ifPresent(index -> {
+            if (taskRepository.existsByIndex(index)) {
+                throw new UniquenessViolationException(List.of("Index %d already exists".formatted(index)));
+            }
+        });
+
+        return taskRepository.findById(id)
+                .map(task -> taskMapper.update(task, updateDto));
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+}
