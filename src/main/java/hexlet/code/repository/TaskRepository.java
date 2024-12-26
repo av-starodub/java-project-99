2cba646aabecf381bc6b4c5ccd15481fc9b2a60a
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-12-26 17:51:15 +0300

    Add SQL queries to the existence check methods in the TaskRepository

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -2,11 +2,21 @@ package hexlet.code.repository;
 
 import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    boolean existsByAssigneeId(Long userId);
+    @Query("SELECT CASE WHEN EXISTS "
+            + "(SELECT 1 FROM Task t WHERE t.assignee.id = :userId) THEN true ELSE false END"
+    )
+    boolean existsByAssigneeId(@Param("userId") Long userId);
 
-    boolean existsByTaskStatusId(Long taskStatusId);
+    @Query("SELECT CASE WHEN EXISTS "
+            + "(SELECT 1 FROM Task t WHERE t.taskStatus.id = :taskStatusId) THEN true ELSE false END"
+    )
+    boolean existsByTaskStatusId(@Param("taskStatusId") Long taskStatusId);
 
 }
